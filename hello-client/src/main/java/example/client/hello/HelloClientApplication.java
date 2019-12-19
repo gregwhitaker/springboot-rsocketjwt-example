844c6708b6862cf5a8088afb5fb892ed34fd0373
gregwhitaker <gwhitake@gmail.com>
2019-12-19 08:07:37 -0800

    Doing work

---
+++ hello-client/src/main/java/example/client/hello/HelloClientApplication.java
@@ -0,0 +1,12 @@
+package example.client.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloClientApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(HelloClientApplication.class, args);
+    }
+}
---
+++ hello-service/src/main/java/example/service/hello/HelloServiceApplication.java
@@ -0,0 +1,12 @@
+package example.service.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServiceApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(HelloServiceApplication.class, args);
+    }
+}
