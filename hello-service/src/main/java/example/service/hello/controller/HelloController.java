b983aa4fa57c20529e28396de8765fd1773cf887
gregwhitaker <gwhitake@gmail.com>
2019-12-19 08:49:02 -0800

    Doing work

--- hello-service/src/main/java/example/service/hello/controller/HelloController.java
+++ hello-service/src/main/java/example/service/hello/controller/HelloController.java
@@ -7,13 +7,36 @@ import reactor.core.publisher.Mono;
 @Controller
 public class HelloController {
 
+    /**
+     * Return a hello message.
+     *
+     * @param name name to put in the hello message
+     * @return hello message
+     */
     @MessageMapping("hello")
     public Mono<String> hello(String name) {
         return Mono.just(String.format("Hello, %s! - from unsecured method", name));
     }
 
+    /**
+     * Return a hello message for any authenticated user.
+     *
+     * @param name name to put in the hello message
+     * @return hello message
+     */
     @MessageMapping("hello.secure")
     public Mono<String> helloSecure(String name) {
         return Mono.just(String.format("Hello, %s! - from secured method", name));
+    }
+
+    /**
+     * Return a hello message only for authenticated admin users.
+     *
+     * @param name name to put in the hello message
+     * @return hello message
+     */
+    @MessageMapping("hello.secure.adminonly")
+    public Mono<String> helloSecureAdminOnly(String name) {
+        return Mono.just(String.format("Hello, %s! - from secured method [admin only]", name));
     }
 }
