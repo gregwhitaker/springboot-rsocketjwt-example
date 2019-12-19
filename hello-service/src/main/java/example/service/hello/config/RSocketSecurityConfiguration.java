c66f618a9c6fe0c0d6fa629f98fae6d94bbb4ef5
gregwhitaker <gwhitake@gmail.com>
2019-12-19 08:23:14 -0800

    Doing work

--- hello-service/src/main/java/example/service/hello/config/RSocketSecurityConfiguration.java
+++ hello-service/src/main/java/example/service/hello/config/RSocketSecurityConfiguration.java
@@ -1,8 +1,40 @@
 package example.service.hello.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.rsocket.EnableRSocketSecurity;
+import org.springframework.security.config.annotation.rsocket.RSocketSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.rsocket.core.PayloadSocketAcceptorInterceptor;
 
 @Configuration
+@EnableRSocketSecurity
 public class RSocketSecurityConfiguration {
 
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+
+        return new MapReactiveUserDetailsService(user);
+    }
+
+    @Bean
+    public PayloadSocketAcceptorInterceptor rsocketInterceptor(RSocketSecurity rsocket) {
+        rsocket.authorizePayload(authorize ->
+                authorize
+                        .route("hello").permitAll()     // Permit access to the "hello" route
+                        .anyRequest().authenticated()
+                        .anyExchange().permitAll()
+        )
+                .basicAuthentication(Customizer.withDefaults());
+
+        return rsocket.build();
+    }
 }
