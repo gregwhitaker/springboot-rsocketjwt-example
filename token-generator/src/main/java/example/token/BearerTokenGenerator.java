949d2374e9e73cdaf5cbfa0f4a4bf5e9ea4ce489
gregwhitaker <gwhitake@gmail.com>
2019-12-20 07:12:01 -0800

    Doing work

--- token-generator/src/main/java/example/token/BearerTokenGenerator.java
+++ token-generator/src/main/java/example/token/BearerTokenGenerator.java
@@ -1,8 +1,43 @@
 package example.token;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import java.sql.Date;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
+/**
+ * Generates an admin token and user token for the demo.
+ */
 public class BearerTokenGenerator {
 
-    public static void main(String... args) {
+    public static void main(String... args) throws Exception {
+        Algorithm algorithm = Algorithm.HMAC256("demosecret");
 
+        String adminToken = JWT.create()
+                .withJWTId(UUID.randomUUID().toString())
+                .withIssuer("hello-service-demo")
+                .withSubject("admin")
+                .withExpiresAt(Date.from(Instant.now().plus(30, ChronoUnit.MINUTES)))
+                .withAudience("hello-service")
+                .sign(algorithm);
+
+        String userToken = JWT.create()
+                .withJWTId(UUID.randomUUID().toString())
+                .withIssuer("hello-service-demo")
+                .withSubject("user")
+                .withExpiresAt(Date.from(Instant.now().plus(30, ChronoUnit.MINUTES)))
+                .withAudience("hello-service")
+                .sign(algorithm);
+
+        System.out.println();
+        System.out.println("Generated Tokens");
+        System.out.println("================");
+        System.out.println("Admin: \n" + adminToken);
+        System.out.println();
+        System.out.println("User: \n" + userToken);
+        System.out.println();
     }
 }
